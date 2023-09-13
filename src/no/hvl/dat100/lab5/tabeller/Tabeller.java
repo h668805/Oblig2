51d557d27604a0e40cf0be1f734ade8a2354681c
h668805 <668805@stud.hvl.no>
2023-09-13 16:17:57 +0200

    Update Tabeller.java

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -4,25 +4,28 @@ public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
+		for (int i = 0; i < tabell.length; i++) {
+			System.out.print(tabell[i] + " ");
+		}
+		System.out.println();
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String tekst = "[";
 		
+		for (int i = 0; i < tabell.length; i++) {
+			tekst += tabell[i];
+			if (i != tabell.length - 1) {
+				tekst += ",";
+			}
+		}
+		return tekst + "]";
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		
 	}
 
 	// d)
